aa3cab4a3d312a0eeca8c5caec213cefec6581ec
yuchen zhao <ge64niy@mytum.de>
2024-02-03 05:53:07 +0100

    delete build file

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nana/GroupB
-# Build directory: /home/nana/GroupB/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("libs/spdlog")
--- build/libs/spdlog/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nana/GroupB/libs/spdlog
-# Build directory: /home/nana/GroupB/build/libs/spdlog
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
